c56e3138b3fe01b4c668ccbe8e3ee6b2fdec1575
yuzuka4573 <yuto457350@gmail.com>
2016-12-09 11:58:10 +0900

    プロジェクト ファイルを追加します。

---
+++ intel_auto_screenshotter/Form1.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace intel_auto_screenshotter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.text1 = new System.Windows.Forms.Label();
+            this.interval = new System.Windows.Forms.TextBox();
+            this.err = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.refe = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.time = new System.Windows.Forms.Label();
+            this.stop = new System.Windows.Forms.Button();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 24);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(72, 69);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Turquoise;
+            this.button1.Location = new System.Drawing.Point(259, 202);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(89, 34);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "CONFIRM";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Transparent;
+            this.button2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button2.BackgroundImage")));
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.Yellow;
+            this.button2.Location = new System.Drawing.Point(354, 202);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(84, 34);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "EXIT";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // text1
+            // 
+            this.text1.AutoSize = true;
+            this.text1.BackColor = System.Drawing.Color.Transparent;
+            this.text1.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.text1.ForeColor = System.Drawing.Color.White;
+            this.text1.Location = new System.Drawing.Point(102, 24);
+            this.text1.Name = "text1";
+            this.text1.Size = new System.Drawing.Size(322, 15);
+            this.text1.TabIndex = 3;
+            this.text1.Text = "Set the interval to take a screen shot (1~60)";
+            // 
+            // interval
+            // 
+            this.interval.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.interval.Location = new System.Drawing.Point(307, 54);
+            this.interval.Name = "interval";
+            this.interval.Size = new System.Drawing.Size(100, 22);
+            this.interval.TabIndex = 4;
+            // 
+            // err
+            // 
+            this.err.AutoSize = true;
+            this.err.BackColor = System.Drawing.Color.Transparent;
+            this.err.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.err.ForeColor = System.Drawing.Color.Red;
+            this.err.Location = new System.Drawing.Point(12, 143);
+            this.err.Name = "err";
+            this.err.Size = new System.Drawing.Size(0, 18);
+            this.err.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(102, 93);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(196, 15);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Set the pass to save image ";
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Transparent;
+            this.button3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button3.BackgroundImage")));
+            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button3.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.Turquoise;
+            this.button3.Location = new System.Drawing.Point(318, 82);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(89, 34);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Reference";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // refe
+            // 
+            this.refe.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.refe.Location = new System.Drawing.Point(105, 119);
+            this.refe.Name = "refe";
+            this.refe.ReadOnly = true;
+            this.refe.Size = new System.Drawing.Size(302, 22);
+            this.refe.TabIndex = 8;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick_1);
+            // 
+            // time
+            // 
+            this.time.AutoSize = true;
+            this.time.BackColor = System.Drawing.Color.Transparent;
+            this.time.Font = new System.Drawing.Font("Consolas", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.time.ForeColor = System.Drawing.Color.White;
+            this.time.Location = new System.Drawing.Point(90, 39);
+            this.time.Name = "time";
+            this.time.Size = new System.Drawing.Size(139, 43);
+            this.time.TabIndex = 9;
+            this.time.Text = "label2";
+            this.time.Visible = false;
+            // 
+            // stop
+            // 
+            this.stop.BackColor = System.Drawing.Color.Transparent;
+            this.stop.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("stop.BackgroundImage")));
+            this.stop.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.stop.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.stop.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.stop.ForeColor = System.Drawing.Color.Turquoise;
+            this.stop.Location = new System.Drawing.Point(15, 202);
+            this.stop.Name = "stop";
+            this.stop.Size = new System.Drawing.Size(89, 34);
+            this.stop.TabIndex = 10;
+            this.stop.Text = "STOP";
+            this.stop.UseVisualStyleBackColor = false;
+            this.stop.Visible = false;
+            this.stop.Click += new System.EventHandler(this.stop_Click);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnablePreventFocusChange;
+            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.CausesValidation = false;
+            this.ClientSize = new System.Drawing.Size(450, 248);
+            this.Controls.Add(this.stop);
+            this.Controls.Add(this.time);
+            this.Controls.Add(this.refe);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.err);
+            this.Controls.Add(this.interval);
+            this.Controls.Add(this.text1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label text1;
+        private System.Windows.Forms.TextBox interval;
+        private System.Windows.Forms.Label err;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox refe;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label time;
+        private System.Windows.Forms.Button stop;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ intel_auto_screenshotter/Form1.cs
@@ -0,0 +1,270 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Permissions;
+using System.Threading;
+using System.Windows.Media.Imaging;
+using System.IO;
+
+
+namespace intel_auto_screenshotter
+{
+    public partial class Form1 : Form
+    {
+        
+
+        [SecurityPermission(SecurityAction.Demand,
+    Flags = SecurityPermissionFlag.UnmanagedCode)]
+        protected override void WndProc(ref Message m)
+        {
+            base.WndProc(ref m);
+
+            const int WM_NCHITTEST = 0x84;
+            const int HTCLIENT = 1;
+            const int HTCAPTION = 2;
+            if ((m.Msg == WM_NCHITTEST) &&
+                (m.Result.ToInt32() == HTCLIENT))
+            {
+                m.Result = (IntPtr)HTCAPTION;
+            }
+        }
+        //varialbes
+        int intervalmin;
+        string pass;
+        int count = 1;
+        int hour=0,minute=0;
+        string errm;
+        int butime=0;
+        DateTime dtNow= DateTime.Now;
+        System.Windows.Forms.Timer timer;
+        string[] ipass = new string[9999];
+        int month, day, h, m;
+        List<string> imageh = new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.MaximumSize = this.Size;
+            
+            month = dtNow.Month;
+            day = dtNow.Day;
+            h = dtNow.Hour;
+            m = dtNow.Minute;
+            Console.WriteLine("start timer");
+            timer1.Start();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("exit clicked");
+            //exit application
+            Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("CONFIRM button clicked");
+            errm = "";
+            if (interval.Text == "")
+            {
+                intervalmin = 0;
+            }else {
+                intervalmin = int.Parse(interval.Text);
+            }
+
+            if (intervalmin > 60)
+            {
+                Console.WriteLine("get error for over 60");
+                errm = "ERROR \r\nPlease type number of interval less than 60";
+                err.Text = errm;
+            }
+            else if (intervalmin < 1)
+            {
+                Console.WriteLine("get error for less then 1");
+                errm = "ERROR \r\nPlease type number of interval more than 0";
+                err.Text = errm;
+            }
+            if (refe.Text == "")
+            {
+                if (errm != "") errm = errm+"\r\nPlease select the folder to save image";
+                else errm= "ERROR\r\nPlease select the folder to save image";
+                err.Text = errm;
+            }
+            else if (intervalmin > 0 && intervalmin < 61)
+            {
+                if (intervalmin == 60) hour = 1;
+                else minute = intervalmin;
+                
+                err.Text = errm;
+                Console.WriteLine("info");
+                Console.WriteLine("interval : " + intervalmin);
+                Console.WriteLine("folder pass : " + pass);
+                TopMost = true;
+                //hide forms
+                text1.Hide();
+                interval.Hide();
+                err.Hide();
+                label1.Hide();
+                button3.Hide();
+                refe.Hide();
+                button1.Hide();
+                time.Show();
+                stop.Show();
+                //screen_shot();
+                timer = new System.Windows.Forms.Timer();
+                timer.Tick += new EventHandler(screen_shot);
+                timer.Interval = intervalmin*60000;
+                timer.Enabled = true; // timer.Start()と同じ
+                //record start date
+                month = dtNow.Month;
+                day = dtNow.Day;
+                h = dtNow.Hour;
+                m = dtNow.Minute;
+            }  
+        }
+        private void screen_shot(object sender, EventArgs e)
+        {
+            Console.WriteLine("running ss sys");
+            Bitmap bm = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+            Graphics gr = Graphics.FromImage(bm);
+            //getting date value
+            dtNow = DateTime.Now;
+            int m = dtNow.Month;
+            int d = dtNow.Day;
+            int h = dtNow.Hour;
+            int min = dtNow.Minute;
+            gr.CopyFromScreen(new Point(0, 0), new Point(0, 0), bm.Size);
+            bm.Save(pass + "\\" + m.ToString("00") + "_" + d.ToString("00") + "_" + h.ToString("00") + "_" + min.ToString("00") + ".png", System.Drawing.Imaging.ImageFormat.Png);
+            imageh.Add( pass + "\\" + m.ToString("00") + "_" + d.ToString("00") + "_" + h.ToString("00") + "_" + min.ToString("00") + ".png");
+            //update web site
+            SendKeys.SendWait("{F5}");
+            SendKeys.Flush();  
+            bm.Dispose();
+            gr.Dispose();
+            Console.WriteLine("take picture : " + count);
+            count++;
+        }
+       
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("click reference");
+            FolderBrowserDialog folderBrowserDialog1 = new FolderBrowserDialog();
+            folderBrowserDialog1.Description = "Select the directry to save the image";
+            folderBrowserDialog1.RootFolder = System.Environment.SpecialFolder.MyComputer;
+            folderBrowserDialog1.SelectedPath = System.Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+            folderBrowserDialog1.ShowNewFolderButton = true;
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                refe.Text=folderBrowserDialog1.SelectedPath;
+                pass = refe.Text;
+            }
+            folderBrowserDialog1.Dispose();
+        }
+
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+            DateTime datetime = DateTime.Now;
+            time.Text = datetime.ToShortDateString()+" "+ datetime.ToShortTimeString();
+        }
+
+        private void stop_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("click stop timer");
+            TopMost = false;
+            text1.Show();
+            interval.Show();
+            err.Show();
+            label1.Show();
+            button3.Show();
+            refe.Show();
+            button1.Show();
+            time.Hide();
+            stop.Hide();
+            timer2.Stop();
+            timer.Dispose();
+            gif_anime(pass,ipass);
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            DateTime datetime_now = DateTime.Now;
+            int m2 = datetime_now.Minute+minute;
+            int h2 = datetime_now.Hour + hour;
+            DateTime datetime_set = new DateTime(datetime_now.Year, datetime_now.Month, datetime_now.Day, h2, m2-1,30);
+            int time = 0;
+            time = (((datetime_set.Hour - datetime_now.Hour) * 3600) + ((datetime_set.Minute - datetime_now.Minute) * 60) + (datetime_set.Second - datetime_now.Second));
+            if (butime != time)
+            {
+                Console.WriteLine("time left : " + time);
+                butime = time;
+            }
+            if (datetime_now.ToLongTimeString() == datetime_set.ToLongTimeString())
+            {
+                Console.WriteLine("stopping timer 2");
+                timer2.Stop();
+            }
+        }
+        private void gif_anime(string savePath, string[] image) {
+            string animepass = savePath + "//anime_" + month.ToString("00") + "_" + day.ToString("00") + "_" + h.ToString("00") + "_" + m.ToString("00") + ".gif";
+            Boolean err = false;
+            //open write files
+
+            FileStream outputFileStrm = new FileStream(animepass,
+                FileMode.Create, FileAccess.Write, FileShare.None);
+            try
+            {
+                //GifBitmapEncoderを作成する
+                Console.WriteLine("start gif sys");
+                GifBitmapEncoder encoder = new GifBitmapEncoder();
+                Console.WriteLine("array num : " + imageh.Count);
+                string[] imageFiles = new string[imageh.Count];
+                //copy
+                for (int c1 = 0; c1 < imageh.Count; c1++)
+                {
+                    imageFiles[c1] = imageh[c1];
+                }
+                
+                    foreach (string f in imageFiles)
+                    {
+
+                        //画像ファイルからBitmapFrameを作成する
+                        BitmapFrame bmpFrame =
+                            BitmapFrame.Create(new Uri(f, UriKind.RelativeOrAbsolute));
+                        //フレームに追加する
+                        encoder.Frames.Add(bmpFrame);
+                    }
+
+
+                //save
+                encoder.Save(outputFileStrm);
+                //close
+                outputFileStrm.Close();
+
+                Array.Clear(ipass, 0, 9999);
+                imageh.Clear();
+            }
+            catch (Exception e)
+            {
+                outputFileStrm.Close();
+                Console.WriteLine("getting some error");
+                Console.WriteLine(e);
+                err = true;
+            }
+            if (err == true) {
+                
+                File.Delete(animepass);
+            }
+        }
+    }
+}
